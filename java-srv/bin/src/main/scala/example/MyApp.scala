f5354ad80f527804872a146de6b99f009f0f019e
bearddan2000 <bearddan2000@yahoo.com>
2021-04-06 15:49:44 -0400

    initial commit

---
+++ java-srv/bin/src/main/scala/example/MyApp.scala
@@ -0,0 +1,14 @@
+package example;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+object MyApp {
+
+  def main(args: Array[String]): Unit = {
+    val plainText = "Hello World!";
+    val stored = BCrypt.hashpw(plainText, BCrypt.gensalt());
+    println("Original: " + plainText);
+    println("Hash: " + stored);
+    println("Verified: " + BCrypt.checkpw(plainText, stored));
+  }
+}
